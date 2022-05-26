1205ac49b141dc7bdf2ffcb112ee0be804e8a3b8
imvikasmandal <imvikasmandal@gmail.com>
2022-05-26 07:18:37 -0500

    Updated Diff.java

--- Diff.java
+++ Diff.java
@@ -1,11 +1,10 @@
-'''This is a message'''
-public class sum
+public class diff
 {
     public static void main(String[] args) {
       int a = 5;
-      int b = 6;
+      int b = 7;
 
-      System.out.println("Print diff is"+ (a-b));
+      System.out.println("Print diff is  "+ (a-b));
 
     }
 }
