6bc5b975a4bfeba3ab4c74b0f47ebac246fcf0cd
imvikasmandal <imvikasmandal@gmail.com>
2022-05-26 06:35:06 -0500

    Changed name

---
+++ Diff.java
@@ -0,0 +1,11 @@
+'''This is a message'''
+public class sum
+{
+    public static void main(String[] args) {
+      int a = 5;
+      int b = 6;
+
+      System.out.println("Print diff is"+ (a-b));
+
+    }
+}
